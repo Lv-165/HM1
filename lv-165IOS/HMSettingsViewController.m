48412794612ec0a48f0ccb990dd2c77cd29ac42f
Ihor Zabrotsky <ihor.zabrotsky@gmail.com>
2015-11-30 18:16:00 +0200

    NavigationController added SettingsViewController added Segue to SettingsViewController added

---
+++ lv-165IOS/HMSettingsViewController.m
@@ -0,0 +1,37 @@
+//
+//  HMSettingsViewController.m
+//  lv-165IOS
+//
+//  Created by Ihor Zabrotsky on 11/30/15.
+//  Copyright © 2015 SS. All rights reserved.
+//
+
+#import "HMSettingsViewController.h"
+
+@interface HMSettingsViewController ()
+
+@end
+
+@implementation HMSettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
